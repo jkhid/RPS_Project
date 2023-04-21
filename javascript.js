f2ab30bc86aec6b8a011be225fbf1ba019b93dd4
Jamal Khidir <116044175+jkhid@users.noreply.github.com>
2023-04-21 11:06:29 -0700

    Create playRound function - Rock portion

--- javascript.js
+++ javascript.js
@@ -1,9 +1,25 @@
-// Create getComputerChoice function
 // Create an array to store option values
 // Create function that randomly selects value from array
-const choices = ["Rock", "Paper", "Scissors"];
+const choices = ["rock", "paper", "scissors"];
 function getComputerChoice(){
     const computerChoice = choices[Math.floor(Math.random() * choices.length)];
     return computerChoice;
 }
-console.log(getComputerChoice());
+
+// Create a function that plays a round of RPS
+// The function takes in two parameters, player and computer
+// Return a string that declares a winner/loser
+function playRound() {
+    const playerSelection = prompt("Enter Rock, Paper, or Scissors").toLowerCase();
+    const computerSelection = getComputerChoice();
+    console.log(computerSelection)
+
+    if (playerSelection === "rock" && computerSelection === "paper") {
+        alert("You lose! Paper beats rock!")
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        alert("You win! Rock beats Scissors!")
+    } else if (playerSelection === "rock" && computerSelection === "rock") {
+        alert("Tie! Rock equals Rock!")
+    }
+}
+playRound()
